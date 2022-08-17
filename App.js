4b85fd4777bc253a8616d4e9975314039e70ab12
Rajesh Verma <rajeshverma@Rajeshs-MacBook-Pro-2.local>
2022-08-17 19:03:22 +0530

    Persit store added

--- App.js
+++ App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation';
 import {getStore, getPersistor} from '@redux/index';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {StyledText} from '@components/atoms';
 
 const App = () => {
   const store = getStore();
   const persistor = getPersistor();
 
+  const onBeforeLift = () => {
+    //Do some stuff that when redux has initialized
+  };
+
   return (
     <SafeAreaProvider>
-      <AppNavigator />
+      <Provider store={store}>
+        <PersistGate
+          loading={<StyledText>Loading...</StyledText>}
+          persistor={persistor}
+          onBeforeLift={onBeforeLift}>
+          <AppNavigator />
+        </PersistGate>
+      </Provider>
     </SafeAreaProvider>
   );
 };
